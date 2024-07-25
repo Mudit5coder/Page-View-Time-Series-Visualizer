f61f45ade912311097a9f06efb0ae58b7b798f15
Mudit5coder <161054031+Mudit5coder@users.noreply.github.com>
2024-07-26 02:41:23 +0530

    Add files via upload

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -17,12 +17,13 @@ df = df[(df['value'] >= lower_bound) & (df['value'] <= upper_bound)]
 
 
 def draw_line_plot():
-    plt.figure(figsize=(12, 6))  # Set the figure size
-    plt.plot(df["date"], df["value"])
+    fig, ax = plt.subplots(figsize=(12, 6)) # Set the figure size
+    ax.plot(df.index, df["value"], color='tab:red', linewidth=1)
     plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
     plt.xlabel('Date')
     plt.ylabel('Page Views')
-    plt.show()
+    
+    
 
 
 
@@ -34,7 +35,17 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df['month'] = df.index.month
+    df['year'] = df.index.year
+
+    df_bar = df.groupby(['year', 'month'])['value'].mean().unstack()
+
+    fig, ax = plt.subplots(figsize=(12, 6)) # Set the figure size
+    df_bar.plot(kind='bar', ax=ax)
+    plt.title('Average Page Views')
+    plt.xlabel('Years')
+    plt.ylabel('Average Page Views')
+    plt.legend(title='Months')
 
     # Draw bar plot
 
@@ -47,14 +58,31 @@ def draw_bar_plot():
     return fig
 
 def draw_box_plot():
-    # Prepare data for box plots (this part is done!)
+    # Prepare data for box plots 
     df_box = df.copy()
     df_box.reset_index(inplace=True)
     df_box['year'] = [d.year for d in df_box.date]
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
 
+    sns.boxplot(x='year', y='value', data=df, ax=axes[0])
+    axes[0].set_title('Year-wise Box Plot (Trend)')
+    axes[0].set_xlabel('Year')
+    axes[0].set_ylabel('Page Views') 
+
+
+    # Month-wise box plot
+    sns.boxplot(x='month', y='value', data=df, ax=axes[1])
+    axes[1].set_title('Month-wise Box Plot (Seasonality)')
+    axes[1].set_xlabel('Month')
+    axes[1].set_ylabel('Page Views') 
+
+    axes[1].set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+
+    plt.tight_layout()
+    
 
 
 
